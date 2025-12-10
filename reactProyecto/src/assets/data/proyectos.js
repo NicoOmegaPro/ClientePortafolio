b22595be5643ff1b134f9e6d0ccfeded2551caa3
NicoOmegaPro <launinonino@gmail.com>
2025-12-10 01:31:15 +0100

    'fotos3'

--- reactProyecto/src/assets/data/proyectos.js
+++ reactProyecto/src/assets/data/proyectos.js
@@ -1,63 +1,60 @@
-import ImagenRoblox from "../imagenes/ImagenRoblox.png";
-import CalculadoraPRO from "../imagenes/CalculadoraPRO.png";
-import BlogViaje from "../imagenes/BlogViaje.png";
-import PaginaNetflix from "../imagenes/PaginaNetflix.png";
-import TiendaZapas from "../imagenes/TiendaZapas.png";
-import NicolauDatabase from "../imagenes/NicolauDatabase.png";
+// src/data/proyectos.js
+
+import ImagenRoblox from "/src/assets/data/imagenes/ImagenRoblox.png"
+import CalculadoraPRO from "/src/assets/data/imagenes/CalculadoraPRO.png"
+import BlogViaje from "/src/assets/data/imagenes/BlogViaje.png"
+import PaginaNetflix from "/src/assets/data/imagenes/PaginaNetflix.png"
+import TiendaZapas from "/src/assets/data/imagenes/TiendaZapas.png"
+import NicolauDatabase from "/src/assets/data/imagenes/NicolauDatabase.png"
+
 
 export const proyectosJs = [
-  {
-    id: 1,
-    title: "Roblox Game",
-    tech: "Lua",
-    description:
-      "Juego desarrollado dentro de la plataforma Roblox utilizando el lenguaje Lua. Combina mecánicas de The Floor is Lava con elementos de carreras.",
-    image: ImagenRoblox,
-    alt: "Captura del juego Roblox",
-  },
-  {
-    id: 2,
-    title: "Calculadora",
-    tech: "React · HTML5 & CSS3",
-    description:
-      "Calculadora utilizable en navegadores modernos, con todas las funciones básicas de una calculadora simple como sumar, restar, multiplicar o dividir.",
-    image: CalculadoraPRO,
-    alt: "Captura de la calculadora",
-  },
-  {
-    id: 3,
-    title: "Pagina Netflix",
-    tech: "React · HTML5 & CSS3",
-    description:
-      "Recreación de la página oficial de Netflix, utilizando React para la estructura del HTML5 y el CSS3 para el diseño y los estilos.",
-    image: PaginaNetflix,
-    alt: "Captura de la página de Netflix",
-  },
-  {
-    id: 4,
-    title: "Proyecto PHP",
-    tech: "PHP · HTML5 & CSS3",
-    description:
-      "Proyecto de base de datos realizado con PHP, HTML5 y CSS3. Permite gestionar y organizar todos los datos pertenecientes a la base de datos.",
-    image: NicolauDatabase,
-    alt: "Captura del Proyecto de PHP",
-  },
-  {
-    id: 5,
-    title: "Blog de Viajes",
-    tech: "React · HTML5 & CSS3",
-    description:
-      "Blog de viajes con diseño atractivo y funcional, realizado con React. Tiene apartados para contactar con nosotros y para obtener información sobre nosotros.",
-    image: BlogViaje,
-    alt: "Captura de la página de Blog de Viajes",
-  },
-  {
-    id: 6,
-    title: "Tienda de Zapatos",
-    tech: "React · HTML5 & CSS3",
-    description:
-      "Tienda online de zapatos, realizada con React. Dispone de un diseño moderno y especifica cuando un producto se ha quedado sin stock.",
-    image: TiendaZapas,
-    alt: "Captura de la tienda online de zapatos",
-  },
+    {
+        "id": 1,
+        "title": "Roblox Game",
+        "tech": "Lua",
+        "description": "Juego desarrollado dentro de la plataforma Roblox utilizando el lenguaje Lua. Combina mecánicas de The Floor is Lava con elementos de carreras.",
+        "image": "/src/assets/data/imagenes/ImagenRoblox.png",
+        "alt": "Captura del juego Roblox"
+    },
+    {
+        "id": 2,
+        "title": "Calculadora",
+        "tech": "React · HTML5 & CSS3",
+        "description": "Calculadora utilizable en navegadores modernos, con todas las funciones básicas de una calculadora simple como sumar, restar, multiplicar o dividir.",
+        "image": "/src/assets/data/imagenes/CalculadoraPRO.png",
+        "alt": "Captura de la calculadora"
+    },
+    {
+        "id": 3,
+        "title": "Pagina Netflix",
+        "tech": "React · HTML5 & CSS3",
+        "description": "Recreación de la página oficial de Netflix, utilizando React para la estructura del HTML5 y el CSS3 para el diseño y los estilos.",
+        "image": "/src/assets/data/imagenes/PaginaNetflix.png",
+        "alt": "Captura de la página de Netflix"
+    },
+    {
+        "id": 4,
+        "title": "Proyecto PHP",
+        "tech": "PHP · HTML5 & CSS3",
+        "description": "Proyecto de base de datos realizado con PHP, HTML5 y CSS3. Permite gestionar y organizar todos los datos pertenecientes a la base de datos.",
+        "image": "/src/assets/data/imagenes/NicolauDatabase.png",
+        "alt": "Captura del Proyecto de PHP"
+    },
+    {
+        "id": 5,
+        "title": "Blog de Viajes",
+        "tech": "React · HTML5 & CSS3",
+        "description": "Blog de viajes con diseño atractivo y funcional, realizado con React. Tiene apartados para contactar con nosotros y para obtener información sobre nosotros.",
+        "image": "/src/assets/data/imagenes/BlogViaje.png",
+        "alt": "Captura de la página de Blog de Viajes"
+    },
+    {
+        "id": 6,
+        "title": "Tienda de Zapatos",
+        "tech": "React · HTML5 & CSS3",
+        "description": "Tienda online de zapatos, realizada con React. Dispone de un diseño moderno y especifica cuando un producto se ha quedado sin stock.",
+        "image": "/src/assets/data/imagenes/TiendaZapas.png",
+        "alt": "Captura de la tienda online de zapatos"
+    }
 ];
